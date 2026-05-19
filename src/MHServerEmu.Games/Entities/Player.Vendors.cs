a4a1982ec29d35e7ac3cd051cf495492657d04ef
galoxplz <gael.rossell@gmail.com>
2026-05-19 10:03:47 +0200

    Restore Rift presentation launcher item

--- src/MHServerEmu.Games/Entities/Player.Vendors.cs
+++ src/MHServerEmu.Games/Entities/Player.Vendors.cs
@@ -1146,8 +1146,10 @@ namespace MHServerEmu.Games.Entities
             if (itemSpec == null)
                 return Logger.WarnReturn(false, $"TryAddMythicRiftVendorItem(): Failed to create ItemSpec for {itemProtoRef.GetNameFormatted()}");
 
+            itemSpec = MythicRiftItemPresentation.ApplyLauncherPresentation(itemSpec);
+
             using EntitySettings settings = ObjectPoolManager.Instance.Get<EntitySettings>();
-            settings.EntityRef = itemProtoRef;
+            settings.EntityRef = itemSpec.ItemProtoRef;
             settings.ItemSpec = itemSpec;
 
             if (IsInGame == false)
